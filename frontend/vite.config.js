571af9f9d8d7a8de3103be186101c950802e78e7
Nikita Orlovsky <orlovski15555@gmail.com>
2025-12-15 13:11:48 +0300

    mes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,15 +4,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  preview: {
+    host: true,
+    allowedHosts: 'all', // 🔥 ключевая строка
   },
 })
